48ed81ae906459865ac7eed000027c2721d82ae2
yurtercan <ercanyurt@yahoo.com>
2023-06-05 01:18:25 +0300

    t

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const {randomBytes} =require('crypto');
+const cors = require('cors');
 
 const app =  express();
 app.use(bodyParser.json());
+app.use(cors());
 
 /* initial project phase we keep posts in memory (array) */ 
 const posts = {};
